1c4408cf55f256866610a6e2663a3be42a8465a2
DJTunafish <erik.jungmark@hotmail.com>
2017-02-28 17:22:53 +0100

    Flyttade på deklaration av headers i app.js
    Flyttade på deklarationen av headers i app.js då detta måste göras innan man fixar med routing

--- server/app.js
+++ server/app.js
@@ -42,6 +42,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
+    next();
+});
 
 //ROUTING SETUP
 app.use('/course', course);
@@ -56,12 +62,7 @@ app.listen(3000, function() {
     console.log("Listening at port 3000");
 });
 
-app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
-    next();
-});
+
 
 app.use(function(err, req, res, next) {
     var err = new Error('Not Found');
