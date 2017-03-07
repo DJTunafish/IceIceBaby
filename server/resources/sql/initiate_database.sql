6429619b67f5e74fbc3a18565b4e1be91de7f284
Robert Krook <oamannen@yahoo.com>
2017-03-07 13:10:10 +0100

    Can now retrieve all the members that are not in a group for a given course in the /course/ungrouped resource, and can also retrieve all information about members in a group in the /course/group resource. You need to be logged in to successfully do both of these calls right now.

--- server/resources/sql/initiate_database.sql
+++ server/resources/sql/initiate_database.sql
@@ -3,5 +3,6 @@ drop trigger if exists Groups.group_check;
 drop table User, Student, Admin, Course, Question, Groups, RegisteredAt;
 
 source create_tables.sql;
+source views.sql;
 source triggers.sql;
 source dummydata.sql;
