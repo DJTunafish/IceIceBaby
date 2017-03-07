79284f479f770a49476f60d5d6d87ac9805b02a1
DJTunafish <jungmark.erik@gmail.com>
2017-03-07 14:26:20 +0100

    Slight tweak to views.sql to add more info

--- server/resources/sql/views.sql
+++ server/resources/sql/views.sql
@@ -7,11 +7,11 @@
   is not a part of any group in the course.
 */
 CREATE OR REPLACE VIEW ungroupedStudents AS
-    SELECT cid,RegisteredAt.course
-    FROM Student,RegisteredAt
+    SELECT Student.cid,RegisteredAt.course, firstname,lastname,email,profile
+    FROM Student,RegisteredAt,User
     WHERE
-    Student.cid = RegisteredAt.student
-    AND NOT EXISTS (SELECT * FROM Groups WHERE Groups.student = cid AND Groups.course = RegisteredAt.course);
+    Student.cid = RegisteredAt.student AND User.cid=Student.cid
+    AND NOT EXISTS (SELECT * FROM Groups WHERE Groups.student = Student.cid AND Groups.course = RegisteredAt.course);
 
 /*
   This view simply lists groupmembers and some additional information about them.
